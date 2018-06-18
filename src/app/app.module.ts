06ca33a3e82beed213f919c08dabcc9185867405
klumba12 <dimitry.sitnov@gmail.com>
2018-06-18 09:34:24 +0300

    fix basic template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatSelectModule, MatChipsModule } from '@angular/material';
 
+import { DataService } from '../data.service';
 import { AppComponent } from './app.component';
-import { DataService } from './data.service';
 
 import { GridModule } from 'ng2-qgrid';
 import { ThemeModule } from 'ng2-qgrid/theme/material';
 
 @NgModule({
-	imports: [BrowserModule, FormsModule, BrowserAnimationsModule, GridModule, ThemeModule],
 	declarations: [AppComponent],
 	bootstrap: [AppComponent],
-	providers: [DataService]
+	providers: [DataService],
+	imports: [
+		GridModule,
+		ThemeModule,
+		BrowserModule,
+		FormsModule,
+		BrowserAnimationsModule,
+
+		MatButtonModule,
+		MatSelectModule,
+		MatChipsModule
+	],
+
 })
 export class AppModule { }
