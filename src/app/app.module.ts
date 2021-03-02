4940eb898d3b872410a43dd8e253909977c6bae7
Dmitry Sitnov <dimitry.sitnov@gmail.com>
2021-03-02 17:39:45 +0300

    added tsconfig, update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatSelectModule, MatChipsModule, MatIconModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppComponent } from './app.component';
 import { GridModule, PipeModule } from 'ng2-qgrid';
 import { ThemeModule } from 'ng2-qgrid/theme/material';
 
 @NgModule({
-	declarations: [AppComponent],
-	bootstrap: [AppComponent],
-	providers: [],
-	imports: [
-		GridModule,
-		ThemeModule,
-		PipeModule,
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [],
+  imports: [
+    GridModule,
+    ThemeModule,
+    PipeModule,
 
-		BrowserModule,
-		FormsModule,
-		BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
 
-		MatButtonModule,
-		MatSelectModule,
-		MatChipsModule,
-		MatIconModule,
-	],
+    MatButtonModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatIconModule,
+  ],
 })
 export class AppModule { }
