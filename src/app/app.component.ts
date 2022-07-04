96aee743c53876724c296e227ec5f95c275d94a2
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:29:43 +0300

    example/hierarchy-browser-basic/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,70 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService } from '../data.service';
+import { Grid, GridModel, Command, Node } from 'ng2-qgrid';
+
+const EXAMPLE_TAGS = ['hierarchy-browser-basic', 'Table content is folder tree'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  gridModel: GridModel = this.qgrid.model();
+
+  constructor(private qgrid: Grid) {
+    const gridService = qgrid.service(this.gridModel);
+    const root = new Node('$root', 0);
+    (root as any).isVisited = false;
+
+    const tree = [root];
+    const treePipe: typeof qgrid.pipe.view = (memo, context, next) => {
+      memo.nodes = tree;
+      next(memo);
+    };
+
+    this.gridModel
+      .data({
+        pipe: [
+          qgrid.pipe.memo,
+          treePipe,
+          qgrid.pipe.column,
+          qgrid.pipe.view,
+          qgrid.pipe.scene,
+        ],
+      })
+      .group({
+        toggle: new Command({
+          execute: node => {
+            if ((node as any).isVisited) {
+              return;
+            }
+
+            const level = node.level + 1;
+
+            (node as any).isVisited = true;
+
+            node.children = [
+              new Node(`folder [${level}.0] `, level, 'group'),
+              new Node(`folder [${level}.1] `, level, 'group'),
+              new Node(`file [${level}.2] `, level, 'value'),
+              new Node(`file [${level}.3] `, level, 'value'),
+            ];
+
+            gridService.invalidate();
+          },
+        }),
+      })
+      .selection({
+        mode: 'multiple',
+        unit: 'row',
+        area: 'custom',
+        rowKey: (node: Node) => node.key,
+      });
+  }
+}
