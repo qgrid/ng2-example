ee02d1163374fab7ef7ad11006216fcfbb8f91e7
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 11:59:21 +0300

    update version to ng13.3.x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
-
-import { AppComponent } from './app.component';
 import { GridModule, PipeModule } from 'ng2-qgrid';
 import { ThemeModule } from 'ng2-qgrid/theme/material';
-import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,12 +18,11 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     GridModule,
     ThemeModule,
-
+    PipeModule,
     HttpClientModule,
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-
     MatButtonModule,
     MatSelectModule,
     MatChipsModule,
