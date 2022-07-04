086217cacf03f8acc4d0fefc7aaef99c7c26776e
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:30:27 +0300

    example/layer-grid-blank/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService, Atom } from '../data.service';
+import { Observable } from 'rxjs';
+
+const EXAMPLE_TAGS = ['layer-grid-blank', 'Table is empty on startup and data can be loaded using "Load data" button'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  rows: Observable<Atom[]>;
+
+  constructor(private dataService: DataService) {
+  }
+
+  loadData() {
+    this.rows = this.dataService.getAtoms();
+  }
+}
