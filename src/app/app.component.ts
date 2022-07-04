a8b4f6dfc955c2ba513bf3fdf75ce79c934524b9
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:33:59 +0300

    example/select-column-basic/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService, Human } from '../data.service';
+import { Observable } from 'rxjs';
+
+const EXAMPLE_TAGS = ['select-column-basic', 'Column can be selected by clicking on any cell in it'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  rows: Observable<Human[]>;
+
+  constructor(dataService: DataService) {
+    this.rows = dataService.getPeople();
+  }
+}
