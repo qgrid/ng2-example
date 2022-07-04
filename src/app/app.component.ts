97410875a91a80dc2283c46d9e038f9a05b2f33b
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:32:30 +0300

    example/pivot-column-basic/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService, Atom } from '../data.service';
+import { Observable } from 'rxjs';
+
+const EXAMPLE_TAGS = ['pivot-column-basic', 'Different implementations of pivot columns'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  rows: Observable<Atom[]>;
+
+  constructor(dataService: DataService) {
+    this.rows = dataService.getAtoms();
+  }
+}
