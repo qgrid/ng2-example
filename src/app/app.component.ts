a0349dfb0c4c78a99221c5f65fa3861e009e426e
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:31:19 +0300

    example/look-quotes-model/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService } from '../data.service';
+import { GridModel, Grid } from 'ng2-qgrid';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static id = 'look-quotes-model';
+
+  gridModel: GridModel;
+
+  constructor(dataService: DataService, qgrid: Grid) {
+    this.gridModel = qgrid.model();
+
+    dataService
+      .getQuotes()
+      .subscribe(rows => this.gridModel.data({ rows }));
+  }
+}
