d6d5924a7e1018e733cb0a56af4310cc5dc9f106
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:34:45 +0300

    example/size-column-absolute/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService, Atom } from '../data.service';
+import { Observable } from 'rxjs';
+
+const EXAMPLE_TAGS = ['size-column-absolute', 'Columns have absolute width'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  rows: Observable<Atom[]>;
+
+  constructor(dataService: DataService) {
+    this.rows = dataService.getAtoms();
+  }
+}
