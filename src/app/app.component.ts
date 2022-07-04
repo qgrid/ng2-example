8cf3231b53a43a8f994ed81ab55e1f90e06c2f1f
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:26:25 +0300

    example/details-row-basic/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService, Atom } from '../data.service';
+import { Observable } from 'rxjs';
+
+const EXAMPLE_TAGS =
+	['details-row-basic', 'Details section of every row can be expanded/collapsed using chevron buttons on corresponding row'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  rows: Observable<Atom[]>;
+
+  constructor(dataService: DataService) {
+    this.rows = dataService.getAtoms();
+  }
+}
