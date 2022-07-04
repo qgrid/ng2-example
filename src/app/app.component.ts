67a19e8c077b01abd3002ec200abb9f839fe7cb0
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:28:36 +0300

    example/generate-column-deep/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService, Human } from '../data.service';
+import { Observable } from 'rxjs';
+
+const EXAMPLE_TAGS = ['generate-column-deep', 'Column generation based on data source'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  rows: Observable<Human[]>;
+
+  constructor(dataService: DataService) {
+    this.rows = dataService.getPeople();
+  }
+}
