d2e0dab81432bfd64304d711dcdb4675d49520e3
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:28:22 +0300

    example/focus-cell-custom/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, ChangeDetectionStrategy, ViewChild } from '@angular/core';
+import { DataService, Atom } from '../data.service';
+import { Observable } from 'rxjs';
+import { GridComponent, Grid } from 'ng2-qgrid';
+
+const EXAMPLE_TAGS = ['focus-cell-custom', 'Cell can be focused by entering its number into focus input field and clicking "focus" button'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  @ViewChild(GridComponent, { static: true }) grid: GridComponent;
+
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  rows = this.dataService.getAtoms();
+
+  constructor(
+    private dataService: DataService,
+    private qgrid: Grid
+  ) {
+  }
+
+  focus(rowIndex: string) {
+    const { model } = this.grid;
+    const service = this.qgrid.service(model);
+
+    // navigate to the 2nd page to the bottom
+    service.focus(Number.parseInt(rowIndex, 10) - 1);
+  }
+}
