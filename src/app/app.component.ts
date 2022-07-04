0c56e4cb6e52c8e5ac42adab263f0d8d9453abea
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:34:30 +0300

    example/select-row-range/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Grid } from 'ng2-qgrid';
+import { DataService } from '../data.service';
+
+const EXAMPLE_TAGS = ['select-row-range', 'Select Row Range by Mouse Drag'];
+
+@Component({
+  selector: 'example-select-row-basic',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  gridModel = this.qgrid.model();
+  rows$ = this.dataService.getPeople();
+
+  constructor(
+		private dataService: DataService,
+		private qgrid: Grid,
+  ) {
+  }
+}
