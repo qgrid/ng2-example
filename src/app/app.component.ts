008b8309291883df817801333d7643cca23b3c4f
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:27:28 +0300

    example/edit-row-custom/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component, ViewChild, ChangeDetectionStrategy, AfterViewInit } from '@angular/core';
+import { DataService, Human } from '../data.service';
+import { Observable } from 'rxjs';
+import { Command, GridComponent, PaneComponent } from 'ng2-qgrid';
+
+const EXAMPLE_TAGS = ['edit-row-custom', 'Selected row can be edited using UI button "Open pane"'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './example-edit-row-custom.component.html',
+  styleUrls: ['./example-edit-row-custom.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+
+export class AppComponent implements AfterViewInit {
+  @ViewChild(GridComponent, { static: true }) grid: GridComponent;
+  @ViewChild(PaneComponent, { static: true }) pane: PaneComponent;
+
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  rows$: Observable<Human[]>;
+
+  selectedRow: Human;
+
+  openPane = new Command({
+    execute: () => this.pane.open('right'),
+    canExecute: () => !!this.selectedRow,
+  });
+
+  constructor(dataService: DataService) {
+    this.rows$ = dataService.getPeople();
+  }
+
+  ngAfterViewInit() {
+    const { model } = this.grid;
+
+    model.selectionChanged.watch(e => {
+      if (e.hasChanges('items')) {
+        this.selectedRow = e.state.items[0];
+        this.openPane.canExecuteCheck.next();
+      }
+    });
+  }
+}
