47e88c54202fd75c00f4661429afdf65ef2919f3
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:28:08 +0300

    example/floating-rows-basic/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService } from '../data.service';
+import { GridModel, Grid } from 'ng2-qgrid';
+
+const EXAMPLE_TAGS = ['floating-rows-basic', 'Experimental! Rows can be freezed (freezed rows remain on the top, when grid is scrolled)'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  gridModel: GridModel;
+
+  constructor(dataService: DataService, qgrid: Grid) {
+    this.gridModel = qgrid.model();
+
+    dataService
+      .getAtoms()
+      .subscribe(rows => {
+        this.gridModel.data({ rows });
+        this.gridModel.row({
+          pinTop: [rows[0], rows[1]],
+          pinBottom: [rows[rows.length - 1]],
+        });
+      });
+  }
+}
