47794c9d140fca4e2ab3a8dbc02cd6d5f03e57cf
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:24:46 +0300

    example/column-row-options-basic/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService, Atom } from '../data.service';
+import { Observable } from 'rxjs';
+import { Action, Command } from 'ng2-qgrid';
+
+const EXAMPLE_TAGS = ['column-row-options-basic', 'Each row has a button with some options'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  rows: Observable<Atom[]>;
+  rowActions = [
+    new Action(
+      new Command<{ row: Atom }>({
+        execute: cell => window.open(cell.row.source, '_blank'),
+        shortcut: 'alt+g',
+      }),
+      'Goto Wiki',
+      'link',
+    ),
+    new Action(
+      new Command(),
+      '---',
+    ),
+    new Action(
+      new Command({
+        execute: () => alert('hello world'),
+        shortcut: 'alt+h',
+      }),
+      'Hello World',
+    ),
+  ];
+
+  constructor(dataService: DataService) {
+    this.rows = dataService.getAtoms();
+  }
+}
