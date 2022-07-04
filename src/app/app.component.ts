6bd521bf354921f8393f5a5ae5a4f496f10fe47b
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:27:14 +0300

    example/edit-cell-basic/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, ChangeDetectionStrategy, AfterViewInit } from '@angular/core';
+import { DataService, Human } from '../data.service';
+import { Observable } from 'rxjs';
+import { Grid, Command } from 'ng2-qgrid';
+
+const EXAMPLE_TAGS = ['edit-cell-basic', 'Cell values can be edited, "Gender" has a custom edit template'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent implements AfterViewInit {
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  rows$: Observable<Human[]> = this.dataService.getPeople();
+  gridModel = this.qgrid.model();
+
+  commit = new Command<{ newValue: any }>({
+    execute: e => console.log('commit: ' + e.newValue),
+  });
+
+  cancel = new Command({
+    execute: () => console.log('cancel'),
+  });
+
+  constructor(
+    private dataService: DataService,
+    private qgrid: Grid,
+  ) {
+  }
+
+  ngAfterViewInit() {
+    this.gridModel.edit({
+      cancel: this.cancel,
+      commit: this.commit,
+    });
+  }
+}
