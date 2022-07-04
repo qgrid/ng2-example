9e6600f4aa4096c6f929847242a169473d50d099
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:26:48 +0300

    example/details-row-start/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService, Atom } from '../data.service';
+import { Observable } from 'rxjs';
+
+const EXAMPLE_TAGS = ['details-row-start', 'Details section of every row is loaded on startup, but hidden'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  rows: Observable<Atom[]>;
+
+  constructor(dataService: DataService) {
+    this.rows = dataService.getAtoms();
+  }
+}
