8a192c61d8329e670ca64f1bef0a387bfb190847
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:30:17 +0300

    example/interaction-mode-readonly/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService, Atom } from '../data.service';
+import { Observable } from 'rxjs';
+
+const EXAMPLE_TAGS = ['interaction-mode-readonly', 'Interaction with row is available using "Alert" button'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  rows: Observable<Atom[]>;
+
+  constructor(dataService: DataService) {
+    this.rows = dataService.getAtoms();
+  }
+
+  testAttached() {
+    alert(':-)');
+  }
+}
