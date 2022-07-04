5fa80a56b0fa0e478055160105a6ae600632d0c3
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:31:28 +0300

    example/master-details-basic/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService, Human } from '../data.service';
+import { Observable } from 'rxjs';
+import { GridModel, Grid } from 'ng2-qgrid';
+import { map } from 'rxjs/operators';
+
+const EXAMPLE_TAGS = ['master-details-basic', 'Bottom table shows data, related to selected row from upper table'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  rows$: Observable<Human[]>;
+  detailsRows$: Observable<Human[]>;
+
+  gridModel: GridModel;
+  likes: string[] = [];
+
+  constructor(dataService: DataService, qgrid: Grid) {
+    this.gridModel = qgrid.model();
+    this.rows$ = dataService.getPeople();
+
+    this.gridModel
+      .selectionChanged
+      .watch(e => {
+        if (e.hasChanges('items')) {
+          const { items } = e.state;
+          if (items.length) {
+            this.likes = items[0].likes;
+
+            this.detailsRows$ = dataService
+              .getPeople()
+              .pipe(
+                map(people => people
+                  .filter(human => this.likes
+                    .every(like => human.likes.indexOf(like) >= 0))),
+              );
+          }
+        }
+      });
+  }
+}
