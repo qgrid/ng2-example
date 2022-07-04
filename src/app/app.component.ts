66cff7553d056bd8086af0aa609d9970e321bcfe
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:24:12 +0300

    example/column-list-basic/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService, Human } from '../data.service';
+import { Observable } from 'rxjs';
+
+const EXAMPLE_TAGS = ['column-list-basic', 'Grid content is a list, which can be switched using UI buttons'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  group = '';
+  rows$: Observable<Human[]> = this.dataService.getPeople();
+
+  constructor(private dataService: DataService) {}
+}
