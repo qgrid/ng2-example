8beb5a94cb9842876b69dc55e82fe5c6308d1f54
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:35:19 +0300

    example/sort-row-mixed/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService, Human } from '../data.service';
+import { Observable } from 'rxjs';
+
+const EXAMPLE_TAGS = ['sort-row-mixed', 'Rows are multiple sorted by press Shift key'];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static tags = EXAMPLE_TAGS;
+  title = EXAMPLE_TAGS[1];
+
+  rows: Observable<Human[]>;
+
+  constructor(dataService: DataService) {
+    this.rows = dataService.getPeople();
+  }
+}
