9b0eae8d7e447d23824c23add3eb0f7a8fb93b1e
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 18:33:22 +0300

    example/scroll-virtual-group/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService, Atom } from '../data.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  providers: [DataService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  static id = 'scroll-virtual-group';
+
+  rows: Observable<Atom[]>;
+
+  constructor(dataService: DataService) {
+    this.rows = dataService.getAtoms();
+  }
+}
